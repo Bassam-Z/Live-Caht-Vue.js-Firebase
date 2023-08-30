f4d8f558f2afa441a895b033093eafb9e820ce49
Bassam-Z <bassamz.ali@hotmail.com>
2023-08-30 22:25:59 +0200

    "Add useCollection "

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -5,6 +5,7 @@
     v-model="message"
     @keypress.enter.prevent="handleSubmit"
     ></textarea>
+    <div class="error">{{ error }}</div>
   </form>
 </template>
 
@@ -12,11 +13,14 @@
 import { ref } from 'vue'
 import getUser from '@/composables/getUser';
 import { timestamp } from '@/firebase/config';
+import useCollection from '@/composables/useCollection';
 
 export default {
     setup(){
         const {user} = getUser();
         const message = ref('');
+        const {addDoc, error} = useCollection('messages');
+
 
         const handleSubmit = async () => {
             const chat = {
@@ -25,11 +29,13 @@ export default {
                 createdAt: timestamp()
 
             }
-            console.log(chat)
-            message.value = ''
+            await addDoc(chat)
+            if(!error.value){
+                message.value = ''
+            }
         }
 
-        return { message, handleSubmit}
+        return { message, handleSubmit, error}
     }
 
 }
